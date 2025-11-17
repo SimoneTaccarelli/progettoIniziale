a2c400f335aaf9e2045762ea599418ebf12650c2
Simone <simone-taccarelli@hotmail.it>
2025-11-17 12:27:14 +0100

    aggiunto file css per gestione grandezza immagini

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 document.addEventListener('DOMContentLoaded', getProducts);
 
 function getProducts() {
-    const productList = document.querySelector('main');
-    if (!productList) return;
+    const productRow = document.getElementById('products');
+    if (!productRow) return;
 
     fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
+        .then(r => r.json())
         .then(products => {
-            const cards = products.map(product => `
-                <div class="card m-2" style="width: 18rem; display:inline-block; vertical-align: top;">
-                    <img src="${product.image}" class="card-img-top" alt="${product.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.title.substring(0,15 )}...</h5>
-                        <p class="card-text">${product.description.substring(0,15 )}...</p>
-                        <a href="#" class="btn btn-primary">Dettagli</a>
+            productRow.innerHTML = products.map(product => {
+                const title = product.title.length > 40 ? product.title.substring(0, 37) + '...' : product.title;
+                const desc = product.description.length > 80 ? product.description.substring(0, 77) + '...' : product.description;
+                return`
+                <div class="col-6 col-md-4 col-lg-3 d-flex">
+                  <div class="card product-card flex-fill h-100 shadow-sm">
+                    <img src="${product.image}" class="card-img-top product-image" alt="${product.title}">
+                    <div class="card-body d-flex flex-column">
+                      <h6 class="card-title mb-2">${title}</h6>
+                      <p class="card-text small flex-grow-1">${desc}</p>
+                      <a href="#" class="btn btn-primary btn-sm mt-auto">Dettagli</a>
                     </div>
-                </div>
-            `).join('');
-            productList.innerHTML = cards;
+                  </div>
+                </div>`;
+            }).join('');
         })
         .catch(err => {
             console.error('Errore nel caricamento dei prodotti:', err);
-            productList.innerHTML = '<p class="text-danger">Impossibile caricare i prodotti al momento.</p>';
+            productRow.innerHTML = '<div class="col-12"><p class="text-danger text-center">Impossibile caricare i prodotti.</p></div>';
         });
 }
